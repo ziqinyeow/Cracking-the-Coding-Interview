27a1b9361c2d382a2888545c1f16f599a819b569
Yeow Zi Qin <ziqinyeow@gmail.com>
2021-07-26 16:56:19 +0800

    26/7/2021

---
+++ Leetcode/Daily Challenge/July 2021/Week 4/5. Convert Sorted Array to Binary Search Tree/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+
+        node.left = sortedArrayToBST(nums, start, mid - 1);
+
+        node.right = sortedArrayToBST(nums, mid + 1, end);
+
+        return node;
+    }
+}
---
+++ Leetcode/Random Question/Medium/82. Remove Duplicates from Sorted List II/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode current = dummy;
+
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+                current.next = head.next;
+            } else {
+                current = current.next;
+            }
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
